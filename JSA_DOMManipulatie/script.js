304557d1fe104ef5cd44e60264512ee1bfa98171
Mona Reijneke <cooljay.mr@gmail.com>
2021-07-21 14:40:33 +0200

    Add files via upload

--- JSA_DOMManipulatie/script.js
+++ JSA_DOMManipulatie/script.js
@@ -6,33 +6,26 @@ console.log(newLi); //<li></li>
 //Stap 1: event aan buttons
 // btns.addEventListener("click",XXX );
 
-//Stap 2: function on click
+// //Stap 2: function on click
 const addNewAnimal = () => {
   newLi.classList.add("spotted-animals-list-items");
-  newLi.innerHTML = animals; ///hier gaat het fout, denk ik
+  newLi.innerHTML = animal;
   luAsParent.appendChild(newLi);
-  console.log(addNewAnimal);
 };
-// // console.log(luAsParent); //<lu id=.....</lu>
-// //Stap 3: event aan functie koppelen
-// btns.addEventListener("click", addNewAnimal);
+// console.log(luAsParent); //<lu id=.....</lu>
+
+// //Stap 3: event aan functie koppelen elke button
+// btns.forEach(button => addEventListener("click", addNewAnimal));
 
 //stap 4: toegang tot waardes
 btns.forEach((button) => {
-  const animals = button.innerHTML;
-  console.log(animals);
+  let animal = button.innerHTML;
+  console.log(animal);
 });
 
-// Stap 5: creeer nieuwe list item
-// zie boven
-
-// Stap 6: creeer parent element
-// zie boven
-
-// Stap 7: koppel Child (newLi) aan Parent
-luAsParent.appendChild(newLi);
-
-// Array.from(btns).forEach((button1) => {
-//   // const animals1 = button1.innerHTML;
-//   console.log(button1);
-// });
+btns.forEach((button2) => {
+  button2.addEventListener("click", (newLiValue) => {
+    addNewAnimal(animal);
+    console.log(newLiValue);
+  });
+});
